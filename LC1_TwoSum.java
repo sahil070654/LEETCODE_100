93019188d4a2ab274c335568692fd6e7009fec1e
sahil070654 <sadiqsahilbaigmogal2004@gmail.com>
2025-07-28 23:28:38 +0530

    Create LC1_TwoSum.java

---
+++ LC1_TwoSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int n = arr.length;
+        int[] ans = new int[2];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target){
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
