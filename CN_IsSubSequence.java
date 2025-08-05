1f2dcaf863f57981fb86590252dea2aa31dbc6a9
Mogal Sadiq Sahil Baig <sadiqsahilbaigmogal2004@gmail.com>
2025-08-05 21:58:24 +0530

    Create CN_IsSubSequence.java

---
+++ CN_IsSubSequence.java
@@ -0,0 +1,22 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	public static String isSubsequence(String str1, String str2) {    
+    	// Write your code here.
+		int i= 0;
+		int j = 0;
+		while(i<str1.length() && j<str2.length()){
+			if(str1.charAt(i)==str2.charAt(j)){
+				i++;
+			}
+			j++;
+		}	
+		if(i== str1.length()){
+			return "True";
+		}else{
+			return "False";
+		}
+	}
+
+}
